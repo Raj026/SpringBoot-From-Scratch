923d21209b869a7449ef4523f641978b2593f1e0
Raj026 <rajpatani31@gmail.com>
2024-06-09 11:45:56 +0530

    Saved Multiple objects using UserRepository

--- spring_boot_tut/src/main/java/com/example/springboot_tut/spring_boot_tut/SpringBootTutApplication.java
+++ spring_boot_tut/src/main/java/com/example/springboot_tut/spring_boot_tut/SpringBootTutApplication.java
@@ -1,5 +1,7 @@
 package com.example.springboot_tut.spring_boot_tut;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,16 +16,40 @@ public class SpringBootTutApplication {
 		ApplicationContext context = SpringApplication.run(SpringBootTutApplication.class, args);
 		UserRepository userRepository = context.getBean(UserRepository.class);
 
-		User user = new User();
-		user.setId(1);
-		user.setName("Raj Patani");
-		user.setCity("Ahmedabad");
-		user.setStatus("I am a java programmer");
+		// User user = new User();
+		// user.setId(1);
+		// user.setName("Raj Patani");
+		// user.setCity("Ahmedabad");
+		// user.setStatus("I am a java programmer");
 
-		User user1 = userRepository.save(user);
+		// User user1 = userRepository.save(user);
 
-		System.out.println(user1);
+		// System.out.println(user1);
 
+		//create object of user
+
+		User user1 = new User();
+		user1.setCity("Delhi");
+		user1.setName("Seenu");
+		user1.setStatus("Pyhton programmer");
+
+		User user2 = new User();
+		user2.setCity("Ayodhya");
+		user2.setName("Ram");
+		user2.setStatus("Rust programmer");
+
+		// Saving single user
+		// User resultUser = userRepository.save(user1);
+
+		// System.out.println("saved user" + resultUser);
+		List<User> users = List.of(user1, user2);
+		//save multiple objects
+		Iterable<User> result = userRepository.saveAll(users);
+
+		result.forEach(user -> {
+			System.out.println(user);
+		});
+		System.out.println("Done");
 	}
 
 }
