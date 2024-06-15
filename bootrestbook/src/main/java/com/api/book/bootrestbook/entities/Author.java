bc6cc98b28919acaf92e1e433e186ab388bdec26
Raj026 <rajpatani31@gmail.com>
2024-06-15 18:02:34 +0530

    Performed bi-directional mapping and used JSONBackRef so as to avoid recurring displaying of objects

--- bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Author.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Author.java
@@ -1,10 +1,14 @@
 package com.api.book.bootrestbook.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,6 +23,12 @@ public class Author {
     @Column(name = "last_name")
     private String lastName;
     private String language;
+
+    @OneToOne(mappedBy = "author")
+    @JsonBackReference
+    // as the one to one mapping in book created a column `author_author_id` now, in bidirectional we also want to create a book_id in author
+    //table so to avoid the creation of the extra column in author table we indicate the mapped by author such that the extra column is not created. 
+    private Books book;
 
     public int getAuthorId() {
         return authorId;
@@ -59,6 +69,14 @@ public class Author {
     public String toString() {
         return "Author [authorId=" + authorId + ", firstName=" + firstName + ", lastName=" + lastName + ", language="
                 + language + "]";
+    }
+
+    public Books getBook() {
+        return book;
+    }
+
+    public void setBook(Books book) {
+        this.book = book;
     }
 
 }
--- bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Books.java
+++ bootrestbook/src/main/java/com/api/book/bootrestbook/entities/Books.java
@@ -1,5 +1,7 @@
 package com.api.book.bootrestbook.entities;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -20,6 +22,7 @@ public class Books {
 
     //Book has author but no book object in author thus we will perform uni-directional mapping
     @OneToOne(cascade = CascadeType.ALL)
+    @JsonManagedReference // this will avoid recursion of book and authors as this is parent model otherwise both will be displayed continuosly.
     private Author author;
 
     public Books(int id, String title, Author author) {
